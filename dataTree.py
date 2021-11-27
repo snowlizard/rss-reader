a0edba2a14dd70fa25f7e2b6808ce9af9e5b91fa
snowlizard <illegal_burrito@live.com>
2021-11-26 21:47:28 -0800

    cleaning up

--- dataTree.py
+++ dataTree.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialogButtonBox, QLabel, QMessageBox, QTreeView
+from PyQt5.QtWidgets import QMessageBox, QTreeView
 from dialogA import AddFeedDialog
 from readRSS import ReadRSS
 
